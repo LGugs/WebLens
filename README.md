2fad1788a753111689afa86086df48166322e829
LGugs <lgpa@icomp.ufam.edu.br>
2018-05-03 19:27:30 -0400

    Criação do WebLens

---
+++ README.md
@@ -0,0 +1,3 @@
+## WebLens
+
+Aplicação web direcionada a sintetizar informações de sites de notícias e afins.
---
+++ getInfo.php
@@ -0,0 +1,20 @@
+<?php
+
+// variavel que guarda o conteúdo de uma página web
+$url = $_POST["link"];
+
+$dados = file_get_contents($url);
+
+// função que recupera as informações que contem as caracteristicas especificas dentro do $link e joga em $conteudo
+//preg_match_all('/ORES--&gt;(.+)<!--/s', $dados, $conteudo);
+
+// recupera somente o que desejamos
+//$exibir = $conteudo[0][0];
+
+//$retirar = array('mais moedas','atas Copom','mais detalhes','ORES-->','<!--');
+
+//$exibir = str_replace($retirar, '', $exibir);
+
+echo $conteudo;
+
+?>
