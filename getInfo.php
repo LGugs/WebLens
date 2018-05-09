41ed6202d04db4cc1a3c4de8c2740bf63021f5ba
LGugs <lgpa@icomp.ufam.edu.br>
2018-05-09 19:42:11 -0400

    Retorno de array com todos os textos entre as tags <h1>

--- getInfo.php
+++ getInfo.php
@@ -5,8 +5,24 @@ $url = $_POST["link"];
 
 $dados = file_get_contents($url);
 
+//$padrao = "/<h1 ?.*>(.*)<\/h1>/";
+$padrao = "h1";
 // função que recupera as informações que contem as caracteristicas especificas dentro do $link e joga em $conteudo
-//preg_match_all('/ORES--&gt;(.+)<!--/s', $dados, $conteudo);
+/*$matches = preg_match_all($padrao, $dados, $conteudo);
+
+if ($matches >= 1) {
+    //print "casou";
+    //var_dump($conteudo);
+    echo $conteudo[0][0];
+
+} else if ($matches === 0) {
+    print "não casou";
+    var_dump($matches);
+
+} elseif ($matches === false) {
+    print "ocorreu um erro";
+
+}*/
 
 // recupera somente o que desejamos
 //$exibir = $conteudo[0][0];
@@ -15,6 +31,21 @@ $dados = file_get_contents($url);
 
 //$exibir = str_replace($retirar, '', $exibir);
 
-echo $dados;
+//echo $conteudo;
+
+
+function pegaTextoTags($dados, $padrao){
+    $d = new DOMDocument();
+    $d->loadHTML($dados);
+    $return = array();
+    foreach($d->getElementsByTagName($padrao) as $item){
+        $return[] = $item->textContent;
+    }
+    var_dump($return);
+
+}
+
+pegaTextoTags($dados,$padrao);
+
 
 ?>
